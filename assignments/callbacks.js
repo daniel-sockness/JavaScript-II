b6dbca21ecc45957a2e65df7e17b6a83c428da63
daniel-sockness <danielsock@gmail.com>
2019-04-19 19:11:55 -0500

    Update Callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -7,7 +7,7 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   //Given this problem: 
   
   function firstItem(arr, cb) {
-    // firstItem passes the first item of the given array to the callback function.
+    cb(arr)
   }
 
   // Potential Solution:
@@ -24,27 +24,61 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
-
+//Challenge 1
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
 
+getLength(items, function(arrLength) {
+  console.log(arrLength);
+})
+
+// Challenge 2
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length -1]);
 }
 
+last(items, function(lastItem) {
+  console.log(lastItem);
+})
+
+// Challenge 3
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
 
+const summed = function(addNums) {
+  console.log(addNums);
+}
+
+sumNums(3, 4, summed);
+
+// Challenge 4
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
 
+multiplyNums(3, 4, function(mult) {
+  console.log(mult)
+})
+
+// Challenge 5
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if (list.includes(item)) {
+    return cb(true)
+  } else {
+    return cb(false);
+  }
 }
+
+contains("Pencil", items, function(boolean) {
+  console.log(boolean);
+})
 
 /* STRETCH PROBLEM */
 
